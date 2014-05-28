7ea6f2d3a77d31fc64734fee4beaef8d23395593
atpearson <definitelymabe@hotmail.com>
2014-05-27 20:03:36 -0700

    Update Equation.java

--- src/com/finalsinc/arithmattackgame/Equation.java
+++ src/com/finalsinc/arithmattackgame/Equation.java
@@ -4,11 +4,23 @@ public class Equation {
 	int firstValue;
 	int secondValue;
 	int operation;
+	String eq;
 
 	public Equation(int first, int second, int op) {
 		firstValue = first;
 		secondValue = second;
 		operation = op;
+		eq = "";
+		eq.concat(Integer.toString(first));
+		if(op==0)
+			eq.concat("+");
+		else if(op==1)
+			eq.concat("-");
+		else if(op==2)
+			eq.concat("*");
+		else
+			eq.concat("/");
+		eq.concat(Integer.toString(second));
 	}
 
 	public int getFirstValue() {
