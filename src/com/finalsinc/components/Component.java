4f472eb9dcca1bebfbfba068e3f71178291d6eb8
Nikko Medina <nikkomedina@gmail.com>
2014-05-27 19:31:38 -0700

    Controls has numpad code

---
+++ src/com/finalsinc/components/Component.java
@@ -0,0 +1,22 @@
+package com.finalsinc.components;
+
+
+import com.finalsinc.activities.GameActivity;
+
+public abstract class Component {
+
+	protected GameActivity host;
+	
+	public Component(GameActivity ga) {
+		host = ga;
+	}
+
+	public void reconnect(GameActivity ga) {
+		host = ga;
+	}
+
+	public void disconnect() {
+		host = null;
+	}
+	
+}
